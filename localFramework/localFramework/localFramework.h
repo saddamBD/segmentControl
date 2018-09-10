f32484b20508f6816c0b008bec1e1c1755bb974d
saddam <saddam.ruet@gmail.com>
2018-09-10 11:49:17 +0600

    initial commit to local framework

---
+++ localFramework/localFramework/localFramework.h
@@ -0,0 +1,19 @@
+//
+//  localFramework.h
+//  localFramework
+//
+//  Created by Saddam Hossain on 10/9/18.
+//  Copyright © 2018 Saddam Hossain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for localFramework.
+FOUNDATION_EXPORT double localFrameworkVersionNumber;
+
+//! Project version string for localFramework.
+FOUNDATION_EXPORT const unsigned char localFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <localFramework/PublicHeader.h>
+
+
